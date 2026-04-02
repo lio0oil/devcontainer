656cb9bdb6121f2217af9fefe018e76f506c2210
lio0oil <lio0oil@example.com>
2026-04-03 00:37:30 +0900

    fix: fzfのショートカットキーが機能しない問題を修正し、デフォルトシェルをzshに変更

--- .devcontainer/.dotconfig/.scripts/copy_profile.sh
+++ .devcontainer/.dotconfig/.scripts/copy_profile.sh
@@ -13,3 +13,31 @@ ZSH_THEME="devcontainers"
 plugins=(git)
 source $ZSH/oh-my-zsh.sh
 EOF
+
+# zsh-completions
+cat << "EOF" >> ~/.zshrc
+# zsh-completions
+if type brew &>/dev/null; then
+    FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+    autoload -Uz compinit
+    compinit
+fi
+EOF
+
+# zsh-autosuggestions
+cat << "EOF" >> ~/.zshrc
+# zsh-autosuggestions
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+EOF
+
+# zsh-syntax-highlighting
+cat << "EOF" >> ~/.zshrc
+# zsh-syntax-highlighting
+source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+EOF
+
+# fzf
+cat << "EOF" >> ~/.zshrc
+# fzf
+eval "$(fzf --zsh)"
+EOF
--- .devcontainer/.dotconfig/.scripts/zsh_plugin.sh
+++ .devcontainer/.dotconfig/.scripts/zsh_plugin.sh
@@ -7,31 +7,3 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 else
     echo "Oh My Zsh is already installed."
 fi
-
-# zsh-completions
-cat << "EOF" >> ~/.zshrc
-# zsh-completions
-if type brew &>/dev/null; then
-    FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
-    autoload -Uz compinit
-    compinit
-fi
-EOF
-
-# zsh-autosuggestions
-cat << "EOF" >> ~/.zshrc
-# zsh-autosuggestions
-source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-EOF
-
-# zsh-syntax-highlighting
-cat << "EOF" >> ~/.zshrc
-# zsh-syntax-highlighting
-source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-EOF
-
-# fzf
-cat << "EOF" >> ~/.zshrc
-# fzf
-eval "$(fzf --zsh)"
-EOF
